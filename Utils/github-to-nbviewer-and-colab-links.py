0408783551a538a7ad904ba184ef58f3b289b225
Peter Beens <pbeens@gmail.com>
2023-05-21 16:10:48 -0400

    Update github-to-nbviewer-and-colab-links.py

--- Utils/github-to-nbviewer-and-colab-links.py
+++ Utils/github-to-nbviewer-and-colab-links.py
@@ -1,23 +1,38 @@
+'''
+This program takes an ipynb GitHub link and creates the corresponding NBViewer and Colab links.
+
+It does this by replacing parts of the GitHub link with the appropriate domains for NBViewer 
+and Colab.
+
+It then prints the Colab and NBViewer links, as well as a markdown format that can be used to 
+embed them in a document. 
+'''
+
 print('This program takes an ipynb GitHUb link and creates the corresponding NBViewer and Colab links.')
 print()
 
+# Get the GitHub link of the ipynb file from the user
 github_link = input('Enter the GitHub link of the ipynb file: ')
 
+# Create the Colab link by replacing 'github' with 'githubtocolab'
 colab_link = github_link.replace('github', 'githubtocolab')
+# Create the NBViewer link by replacing 'github.com/' with 'nbviewer.org/github/'
 nbviewer_link = github_link.replace('github.com/', 'nbviewer.org/github/')
 
+# TODO Fix Callysto link
+# Create the Callysto link (NOT WORKING!)
+# callysto_link = 'https://hub.callysto.ca/jupyter/hub/user-redirect/git-pull?repo=' + github_link
+
+# Print the Colab and NBViewer links
 print('Colab')
 print(colab_link)
 print('NBViewer')
 print(nbviewer_link)
+# print('Callysto')
+# print(callysto_link)
 
-print('\nMardown:\n')
+# Print the Markdown header
+print('\nMarkdown:\n')
 
+# Print the markdown format that can be used to embed the links
 print(f'[Colab]({colab_link}) | [NBViewer]({nbviewer_link})')
-
- 
-
-
-# https://github.com/pbeens/Data-Analysis/blob/main/Wikipedia/Canadian-Population-Barchart.ipynb
-# https://githubtocolab.com/pbeens/Data-Analysis/blob/main/Wikipedia/Canadian-Population-Barchart.ipynb
-# https://nbviewer.org/github/pbeens/Data-Analysis/blob/main/Wikipedia/Canadian-Population-Barchart.ipynb
